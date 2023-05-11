5ed4f06504ad2a5f68132836d85c9a7545673b3f
Anton <anton.wiblishauser@gmx.de>
2023-05-11 21:26:07 +0200

    create config

--- src/main/kotlin/dev/nyon/compactspawners/CompactSpawners.kt
+++ src/main/kotlin/dev/nyon/compactspawners/CompactSpawners.kt
@@ -1,9 +1,17 @@
 package dev.nyon.compactspawners
 
+import dev.nyon.compactspawners.config.loadConfig
+import kotlinx.serialization.json.Json
 import net.fabricmc.api.ModInitializer
 
+val json = Json {
+    prettyPrint = true
+    encodeDefaults = true
+}
+
+@Suppress("unused")
 class CompactSpawners : ModInitializer {
     override fun onInitialize() {
-        TODO("Not yet implemented")
+        loadConfig()
     }
 }
---
+++ src/main/kotlin/dev/nyon/compactspawners/config/Config.kt
@@ -0,0 +1,34 @@
+package dev.nyon.compactspawners.config
+
+import dev.nyon.compactspawners.json
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
+import net.fabricmc.loader.api.FabricLoader
+import kotlin.io.path.createFile
+import kotlin.io.path.exists
+import kotlin.io.path.readText
+import kotlin.io.path.writeText
+
+@Serializable
+data class Config(
+    var maxMergedSpawners: Int = -1,
+    var maxStoredExp: Int = -1,
+    var maxStoredDrops: Int = -1,
+    var silkBreakSpawners: Boolean = true
+)
+
+var config = Config()
+private val path = FabricLoader.getInstance().configDir.toAbsolutePath().resolve("moredetails.json")
+    .also { if (!it.exists()) it.createFile() }
+
+fun saveConfig() = path.writeText(json.encodeToString(config))
+
+fun loadConfig() {
+    if (path.readText().isEmpty()) {
+        saveConfig()
+        return
+    }
+
+    config = json.decodeFromString(path.readText())
+}
