c9add89f1666f62284b47538f7cd5a3a75929b30
Anton <anton.wiblishauser@gmx.de>
2023-05-17 20:27:55 +0200

    remve old spawner logic

--- src/main/kotlin/dev/nyon/compactspawners/storage/DimensionStorage.kt
+++
@@ -1,5 +0,0 @@
-package dev.nyon.compactspawners.storage
-
-import net.minecraft.resources.ResourceLocation
-
-data class DimensionStorage(val dimension: ResourceLocation, val spawners: MutableList<CompactSpawnerData>)
