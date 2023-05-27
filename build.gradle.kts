a15800ab1c16668f80b366a914baf60290bc7258
Anton <anton.wiblishauser@gmx.de>
2023-05-27 19:37:46 +0200

    refactor code

--- build.gradle.kts
+++ build.gradle.kts
@@ -80,7 +80,7 @@ val changelogText =
 
 modrinth {
     token.set(findProperty("modrinth.token")?.toString())
-    projectId.set("")
+    projectId.set("KRVzVd0T")
     versionNumber.set(project.version.toString())
     versionType.set("release")
     uploadFile.set(tasks["remapJar"])
